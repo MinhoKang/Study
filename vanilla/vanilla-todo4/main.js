3297418fe603785b26cd9cba91da0fa7b0be4f66
Minho Kang <rkdalsgh0106@naver.com>
2024-03-25 14:43:47 +0900

    todo v6

--- vanilla/vanilla-todo4/main.js
+++ vanilla/vanilla-todo4/main.js
@@ -9,7 +9,7 @@ export default class Main {
   todoArr;
   constructor() {
     window.onpopstate = () => this.render();
-    this.isAccept = false;
+    this.isAccept = localStorage.getItem("isAccept") === "true";
     localStorage.setItem("isAccept", this.isAccept);
     this.todoArr = [];
   }
@@ -169,3 +169,20 @@ new Main().render();
 // EX) HANDLEsUBMIT() 이 함수가 PROMISE를 반환하는 비동기 방식으로 구현
 
 // 인자를 숫자를 받아서 2의 배수면 TRUE를 비동기로 리턴하는 함수를 작성하라.
+
+
+// 1. 일단 돌아가게 만든다.
+// 2. 잘 동작하게 만든다. 
+// 3. 빠르게 동작하게 만든다. 
+
+
+// 1. 역할별로 분리
+// 재사용성, 명확성, 가독성, 
+
+
+// 로그인이라는 클래스는 무슨 역할을 하는가?
+// -> 로그인 관련 모든 것을 담당
+// -> 로컬스토리지 관련 유틸을 만들던
+// -> 상태값에 고민을 해보고
+// isLogin 
+
--- vanilla/vanilla-todo4/routes.js
+++ vanilla/vanilla-todo4/routes.js
@@ -2,17 +2,14 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Todo from "./pages/Todo";
 
-const $app = document.querySelector("#app");
+const isAccept = localStorage.getItem("isAccept");
 
-const HomePage = Home;
-const LoginPage = Login;
-const TodoPage = Todo;
 // const HomePage = new Home($app);
 // const LoginPage = new Login($app);
 // const TodoPage = new Todo($app);
 
 export const routes = {
-  "/home": HomePage,
-  "/login": LoginPage,
-  "/todo": TodoPage,
+  "/home": Home,
+  "/login": Login,
+  "/todo": Todo,
 };
