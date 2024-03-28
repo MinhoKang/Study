a97160b658bf66b1354d330f906cdd552c6efb8a
Minho Kang <rkdalsgh0106@naver.com>
2024-03-28 18:09:50 +0900

    home

--- vanilla/vanilla-todo4/src/pages/Home.ts
+++ vanilla/vanilla-todo4/src/pages/Home.ts
@@ -1,24 +1,20 @@
 import { $app, $body } from "../main";
 
 class Home {
-  $element;
-  constructor() {
-    this.$element = document.createElement("div")
-  }
+  constructor() {}
 
   render() {
-   // 자신의 엘리먼트를 만들어서 반환
-   return this.$element.innerHTML = `<h1>Home</h1>`
-
+    this.setContent();
   }
-  // setContent() {
-  //   const content = `<h1>Home</h1>`;
 
-  //   $body.innerHTML = content;
-  //   if ($app) {
-  //     $app.appendChild($body);
-  //   }
-  // }
+  setContent() {
+    const content = `<h1>Home</h1>`;
+
+    $body.innerHTML = content;
+    if ($app) {
+      $app.appendChild($body);
+    }
+  }
 }
 
 export const homePage = new Home();
