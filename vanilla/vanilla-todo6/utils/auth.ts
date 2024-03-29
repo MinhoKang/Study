e37ca4a9cbbcc1aabd6612d4befa07c2dccd48ca
Minho Kang <rkdalsgh0106@naver.com>
2024-03-29 16:48:17 +0900

    localStorageAction

--- vanilla/vanilla-todo6/utils/auth.ts
+++ vanilla/vanilla-todo6/utils/auth.ts
@@ -1,17 +1,17 @@
-import { LocalStorageUtil } from "./localStorage";
 import account from "../json/account.json" assert { type: "json" };
+import { LocalStorageAction } from "./localStorageAction";
 
-const LocalStorageAction = new LocalStorageUtil();
+const localStorageAction = new LocalStorageAction();
 
 class Auth {
   async logout() {
-    await LocalStorageAction.storage("set", "isAccept", false);
+    await localStorageAction.storage("set", "isAccept", false);
     await window.history.pushState({}, "", "/home");
   }
 
   async login(id: string, password: string) {
     if (id === account.id && password === account.password) {
-      await LocalStorageAction.storage("set", "isAccept", true);
+      await localStorageAction.storage("set", "isAccept", true);
       await window.history.pushState({}, "", "/todo");
     } else {
       alert("아이디 또는 비밀번호가 일치하지 않습니다.");
