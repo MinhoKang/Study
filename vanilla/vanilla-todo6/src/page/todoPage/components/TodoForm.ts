058e574f842dd2557d759c5f490d6a48165b10ea
Minho Kang <rkdalsgh0106@naver.com>
2024-04-01 16:54:51 +0900

    add todo notice2

--- vanilla/vanilla-todo6/src/page/todoPage/components/TodoForm.ts
+++ vanilla/vanilla-todo6/src/page/todoPage/components/TodoForm.ts
@@ -5,6 +5,7 @@ export class TodoForm {
   $section: HTMLElement;
   $form: HTMLFormElement;
   $notice: HTMLElement;
+  todoValue: string;
 
   constructor(store: Store, $sectoion: HTMLElement) {
     this.store = store;
@@ -14,27 +15,7 @@ export class TodoForm {
     this.$form.addEventListener("input", this.handleNotice.bind(this));
     this.$notice = document.createElement("p");
     this.$notice.innerHTML = `5글자 이상 입력하세요`;
-  }
-
-  returnContent() {
-    const content = `<h1>Todo</h1>`;
-    this.$section.innerHTML = content;
-    this.$form.innerHTML = `
-    <input id='todoInput' type='text' placeholder='todo 입력' autofocus='true'/>
-    <button id='addBtn' type='submit'>추가</button>
-    `;
-    this.$section.appendChild(this.$form);
-  }
-
-  handleNotice() {
-    const todoInput: HTMLInputElement =
-      this.$section.querySelector("#todoInput")!;
-
-    if (todoInput.value.length < 5) {
-      this.$form.appendChild(this.$notice);
-    } else if (this.$form.contains(this.$notice)) {
-      this.$form.removeChild(this.$notice);
-    }
+    this.todoValue = "";
   }
 
   add(e: Event) {
@@ -47,18 +28,36 @@ export class TodoForm {
     )
       return;
 
-    const todoInput: HTMLInputElement =
-      this.$section.querySelector("#todoInput")!;
-
-    if (!todoInput.value) {
+    if (!this.todoValue.length) {
       alert("내용을 입력하세요");
       return;
     }
 
     this.store.addTodoItem({
       seq: this.store.todoArr.length,
-      content: todoInput.value,
+      content: this.todoValue,
     });
-    todoInput.value = "";
+    this.todoValue = "";
+  }
+
+  handleNotice(e: Event) {
+    const { value } = e.target as any;
+    this.todoValue = value;
+
+    if (value.length < 5) {
+      this.$form.appendChild(this.$notice);
+    } else if (this.$form.contains(this.$notice)) {
+      this.$form.removeChild(this.$notice);
+    }
+  }
+
+  returnContent() {
+    const content = `<h1>Todo</h1>`;
+    this.$section.innerHTML = content;
+    this.$form.innerHTML = `
+    <input id='todoInput' type='text' placeholder='todo 입력' autofocus='true'/>
+    <button id='addBtn' type='submit'>추가</button>
+    `;
+    this.$section.appendChild(this.$form);
   }
 }
