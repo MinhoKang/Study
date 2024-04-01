c5d271ac9273eeb621a4499fbc8d6a7ef5618236
Minho Kang <rkdalsgh0106@naver.com>
2024-04-01 15:10:48 +0900

    refactoring-TodoForm

--- vanilla/vanilla-todo6/src/page/todoPage/components/TodoForm.ts
+++ vanilla/vanilla-todo6/src/page/todoPage/components/TodoForm.ts
@@ -3,47 +3,69 @@ import { Store } from "../../../store/store";
 export class TodoForm {
   store: Store;
   $section: HTMLElement;
+  $form: HTMLFormElement;
 
   constructor(store: Store, $sectoion: HTMLElement) {
     this.store = store;
     this.$section = $sectoion;
+    this.$form = document.createElement("form");
+    this.$form.addEventListener("click", this.add.bind(this));
 
-    // TODO: Form 이벤트로 리팩토링, 클래스명 바꾸기 
+    // TODO: Form 이벤트로 리팩토링, 클래스명 바꾸기
   }
 
   returnContent() {
-    const content = `<h1>Todo</h1>
-      <form>
-        <input id='todoInput' type='text' placeholder='todo 입력' autofocus='true'/>
-        <button id='addBtn' type='submit'>추가</button>
-      </form>
-    `;
+    const content = `<h1>Todo</h1>`;
     this.$section.innerHTML = content;
-    this.init();
+    this.$form.innerHTML = `
+    <input id='todoInput' type='text' placeholder='todo 입력' autofocus='true'/>
+    <button id='addBtn' type='submit'>추가</button>
+    `;
+    this.$section.appendChild(this.$form);
   }
 
-  init() {
-    this.addTodo();
-  }
+  add(e: Event) {
+    e.preventDefault();
+    if (
+      !e.target ||
+      !(e.target instanceof HTMLElement) ||
+      !(e.target.tagName === "BUTTON")
+    )
+      return;
 
-  addTodo() {
-    const addBtn = this.$section.querySelector("#addBtn");
     const todoInput: HTMLInputElement =
       this.$section.querySelector("#todoInput")!;
 
-    if (!addBtn || !todoInput) return;
+    if (!todoInput.value) {
+      alert("내용을 입력하세요");
+      return;
+    }
 
-    addBtn.addEventListener("click", (e: Event) => {
-      e.preventDefault();
-      if (!todoInput.value) {
-        alert("내용을 입력하세요");
-        return;
-      }
-      this.store.addTodoItem({
-        seq: this.store.todoArr.length,
-        content: todoInput.value,
-      });
-      todoInput.value = "";
+    this.store.addTodoItem({
+      seq: this.store.todoArr.length,
+      content: todoInput.value,
     });
+    todoInput.value = "";
   }
+
+  // addTodo() {
+  //   const addBtn = this.$section.querySelector("#addBtn");
+  //   const todoInput: HTMLInputElement =
+  //     this.$section.querySelector("#todoInput")!;
+
+  //   if (!addBtn || !todoInput) return;
+
+  //   addBtn.addEventListener("click", (e: Event) => {
+  //     e.preventDefault();
+  //     if (!todoInput.value) {
+  //       alert("내용을 입력하세요");
+  //       return;
+  //     }
+  //     this.store.addTodoItem({
+  //       seq: this.store.todoArr.length,
+  //       content: todoInput.value,
+  //     });
+  //     todoInput.value = "";
+  //   });
+  // }
 }
