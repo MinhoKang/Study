30ac36f7962f1cf2c24206f61382c833b5966c0b
Minho Kang <rkdalsgh0106@naver.com>
2024-04-03 13:48:15 +0900

    create axios

--- react/react-todo2/src/apis/api.ts
+++ react/react-todo2/src/apis/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `http://192.168.219.186:8000/`;
+const BASE_URL = `http://192.168.219.186:8000`;
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
--- react/react-todo2/src/pages/SignUpPage/SignUpPage.tsx
+++ react/react-todo2/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,9 +1,19 @@
-import useForm from "../../hooks/useForm";
+import { useState } from "react";
 import styles from "./signUpPage.module.scss";
 import cn from "classnames";
+import { singUp } from "../../apis/signUp";
 
 const SignUpPage = () => {
-  const [email, setEmail] = useForm();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const onSignUp = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const result = await singUp(email, password, checkPassword, phoneNumber);
+    console.log(result);
+  };
 
   return (
     <div className={styles.container}>
@@ -14,6 +24,8 @@ const SignUpPage = () => {
           <input
             type="text"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className={styles.input}
             placeholder="EMAIL"
           />
@@ -23,6 +35,8 @@ const SignUpPage = () => {
           <input
             type="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className={styles.input}
             placeholder="PASSWORD"
           />
@@ -32,6 +46,8 @@ const SignUpPage = () => {
           <input
             type="password"
             id="confrimPassword"
+            value={checkPassword}
+            onChange={(e) => setCheckPassword(e.target.value)}
             className={styles.input}
             placeholder="CONFIRM PASSWORD"
           />
@@ -41,13 +57,21 @@ const SignUpPage = () => {
           <input
             type="tel"
             id="phone"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
             className={styles.input}
             placeholder="PHONE NUMBER"
+            // pattern="[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}"
           />
         </label>
       </form>
       <div className={styles.btns}>
-        <button type="submit" form="signUpForm" className={styles.btn}>
+        <button
+          type="submit"
+          form="signUpForm"
+          className={styles.btn}
+          onClick={onSignUp}
+        >
           SIGN UP
         </button>
         <button type="button" className={cn(styles.btn, styles.cancel)}>
