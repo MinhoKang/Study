c302da2a1ac78ff486e5a613f766a9500efe8739
Minho Kang <rkdalsgh0106@naver.com>
2024-04-05 14:14:08 +0900

    edit types

--- react/react-todo2/src/utils/types.ts
+++ react/react-todo2/src/utils/types.ts
@@ -2,13 +2,3 @@ export type TodoObj = {
   id: number;
   todo: string;
 };
-
-export type NewTodo = {
-  message: string;
-  data: {
-    todos: TodoObj[];
-  };
-};
-
-export type AddTodoListFunction = (newTodo: NewTodo) => void;
-
