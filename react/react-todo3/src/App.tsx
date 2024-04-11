747bb90e7c66fcdc38da57974a744a622c6818d0
Minho Kang <rkdalsgh0106@naver.com>
2024-04-11 17:16:55 +0900

    provide

--- react/react-todo3/src/App.tsx
+++ react/react-todo3/src/App.tsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import css from "./styles/app.module.css";
 import Container from "./components/container/Container";
 import ProtectedRouter from "./routers/ProtectedRouter";
+import { ProvideAuth } from "./hooks/useProvideAuth";
 
 function App() {
   //TODO: 라우터 수정(로그인 안 된 경우 여기에 그냥 노출, 로그인은 Protected로)
   return (
     <Container className={css.container}>
-      <Routes>
-        <Route path="/*" element={<ProtectedRouter />} />
-      </Routes>
+      <ProvideAuth>
+        <Routes>
+          <Route path="/*" element={<ProtectedRouter />} />
+        </Routes>
+      </ProvideAuth>
     </Container>
   );
 }
