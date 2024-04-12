8a21fd3d8fe913afefd4e7fe60ad0add1a19a2cd
Minho Kang <rkdalsgh0106@naver.com>
2024-04-12 16:28:19 +0900

    add todo4

--- react/react-todo3/src/main.tsx
+++ react/react-todo3/src/main.tsx
@@ -2,11 +2,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ProvideAuth } from "./hooks/useProvideAuth.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <ProvideAuth>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProvideAuth>
+  <QueryClientProvider client={queryClient}>
+    <ProvideAuth>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </ProvideAuth>
+  </QueryClientProvider>
 );
