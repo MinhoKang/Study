b457b1e81620d2065b40e2a2c08af4453b00ea32
Minho <rkdalsgh0106@naver.com>
2024-06-02 22:52:55 +0900

    문제 해결 패턴

---
+++ algorithm/JavaScript 알고리즘 & 자료구조 마스터 클래스/4. 문제 해결 패턴/slidingWindow.tsx
@@ -0,0 +1,25 @@
+// sliding window
+
+// 문제
+// 배열과 정수를 받는 함수. 정수에 해당하는 연속하는 숫자의 합이 가장 큰 값을 반환
+// naive
+function maxSubarraySum(arr, num) {
+  if ( num > arr.length){
+    return null;
+  }
+  var max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i ++){
+    temp = 0;
+    for (let j = 0; j < num; j++){
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+
+
