6b22ebbc87fc9d5c8143a1154d239c59221845c5
Minho Kang <rkdalsgh0106@naver.com>
2024-08-21 16:54:27 +0900

    검색 기능 구현

--- next/section02/src/pages/search/index.tsx
+++ next/section02/src/pages/search/index.tsx
@@ -1,9 +1,22 @@
 import { SearchableLayout } from "@/components/SearchableLayout";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import { BookItem } from "@/components/BookItem";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetchBooks";
 
-const Index = () => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { q } = context.query;
+
+  const books = await fetchBooks(q as string);
+
+  return { props: { books } };
+};
+
+const Index = ({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div>
       {books.map((book) => (
