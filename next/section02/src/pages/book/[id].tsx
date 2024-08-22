994291d1b23b4d525f5fb88be282101e82fad5f0
Minho Kang <rkdalsgh0106@naver.com>
2024-08-22 13:50:10 +0900

    SSG fallback옵션

--- next/section02/src/pages/book/[id].tsx
+++ next/section02/src/pages/book/[id].tsx
@@ -5,6 +5,7 @@ import {
 } from "next";
 import style from "./[id].module.css";
 import fetchOneBook from "@/lib/fetchOneBook";
+import { useRouter } from "next/router";
 
 export const getStaticPaths = () => {
   return {
@@ -13,18 +14,28 @@ export const getStaticPaths = () => {
       { params: { id: "2" } },
       { params: { id: "3" } },
     ],
-    fallback: false,
+    fallback: true,
+    // false: 404페이지로 이동
+    // "blocking": SSR 방식으로 실시간으로 페이지 생성
+    // true: SSR 방식 but 데이터를 기다리지 않고 폴백 상태의 페이지(데이터 없는 html 페이지)먼저 반환. 데이터는 후속으로 처리
   };
 };
 
 export const getStaticProps = async (context: GetServerSidePropsContext) => {
   const { id } = context.params!;
+
   const book = await fetchOneBook(Number(id));
+
+  if (!book) return { notFound: true };
 
   return { props: { book } };
 };
 
 const Page = ({ book }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩중";
+
   if (!book) return "문제 발생";
 
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
