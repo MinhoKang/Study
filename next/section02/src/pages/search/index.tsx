670dddb5de0d408d828c4e952e0944a3f9cb470b
Minho Kang <rkdalsgh0106@naver.com>
2024-08-22 13:30:43 +0900

    SSG-동적 경로에 적용 getStaticPaths

--- next/section02/src/pages/search/index.tsx
+++ next/section02/src/pages/search/index.tsx
@@ -8,7 +8,9 @@ import {
 } from "next";
 import fetchBooks from "@/lib/fetchBooks";
 
-export const getStaticProps = async (context: GetServerSidePropsContext) => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const { q } = context.query;
   // SSG방식으로는 쿼리 스트링을 불러올 수 없음 -> 서버에서 이미 실행되기 때문에 브라우저의 정보 못 읽음
   // 따라서 SSR방식으로 작동하게 해야 한다.
@@ -18,7 +20,9 @@ export const getStaticProps = async (context: GetServerSidePropsContext) => {
   return { props: { books } };
 };
 
-const Index = ({ books }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Index = ({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div>
       {books.map((book) => (
