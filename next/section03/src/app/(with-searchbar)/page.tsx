ae7559e97c13d0afcc518fbaf23e1c6c89e4c6e2
Minho Kang <rkdalsgh0106@naver.com>
2024-08-22 19:31:47 +0900

    클라이언트 컴포넌트 및 서버 컴포넌트

--- next/section03/src/app/(with-searchbar)/page.tsx
+++ next/section03/src/app/(with-searchbar)/page.tsx
@@ -1,3 +1,13 @@
+import ClientComponent from "./ClientComponent";
+import ServerComponent from "./ServerComponent";
+
 export default function Home() {
-  return <div>INDEX PAGE</div>;
+  return (
+    <div>
+      INDEX PAGE
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
