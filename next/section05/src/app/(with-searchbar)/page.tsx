421b2a5d98994d23e21d979c4cf363824ca12b6e
Minho Kang <rkdalsgh0106@naver.com>
2024-08-23 14:38:14 +0900

    section05 풀 라우트 캐시 폴더 생성

---
+++ next/section05/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,56 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+const AllBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "no-store" }
+  );
+
+  if (!response.ok) return <div>오류 발생...</div>;
+
+  const allbooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allbooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
+
+const RecommendedBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+
+  if (!response.ok) return <div>오류 발생...</div>;
+
+  const recommendedBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {recommendedBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+};
+
+export default async function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecommendedBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
+    </div>
+  );
+}
