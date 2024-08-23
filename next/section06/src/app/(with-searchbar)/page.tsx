4e4d50a21187180c8afe69720ffb8344658a9e4c
Minho Kang <rkdalsgh0106@naver.com>
2024-08-23 17:22:27 +0900

    Suspense를 활용한 컴포넌트 스트리밍

--- next/section06/src/app/(with-searchbar)/page.tsx
+++ next/section06/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,12 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
 const AllBooks = async () => {
+  await delay(1500);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -22,6 +26,8 @@ const AllBooks = async () => {
 };
 
 const RecommendedBooks = async () => {
+  await delay(3000);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -40,16 +46,22 @@ const RecommendedBooks = async () => {
   );
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecommendedBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다</div>}>
+          <RecommendedBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- next/section06/src/app/(with-searchbar)/search/page.tsx
+++ next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,26 +1,34 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: {
-    q?: string;
-  };
-}) {
+async function SearchResult({ q }: { q: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
   );
 
   if (!response.ok) return <div>오류</div>;
   const books: BookData[] = await response.json();
-
   return (
     <div>
       {books.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense key={searchParams.q || ""} fallback={<div>로딩중</div>}>
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
