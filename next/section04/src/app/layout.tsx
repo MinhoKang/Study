49e9fea9f3ee7419567f6683017926df407ffb70
Minho Kang <rkdalsgh0106@naver.com>
2024-08-23 13:58:19 +0900

    리퀘스트 메모이제이션

--- next/section04/src/app/layout.tsx
+++ next/section04/src/app/layout.tsx
@@ -2,6 +2,22 @@ import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
 
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) return <div>오류</div>;
+  const book = await response.json();
+  const bookCount = book.length;
+
+  return (
+    <div>
+      <footer>제작 @winterlood</footer>
+      <div>{bookCount}개의 도서가 있습니다</div>
+    </div>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,7 +31,8 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          {/* <footer>제작 @winterlood</footer> */}
+          <Footer />
         </div>
       </body>
     </html>
