5d395606bc1c994464b1821f5562c5e9bf98f184
Minho Kang <rkdalsgh0106@naver.com>
2024-08-26 11:17:55 +0900

    서버 액션을 활용한 리뷰 작성

--- next/one-bite-next/section07/src/app/book/[id]/page.tsx
+++ next/one-bite-next/section07/src/app/book/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import style from "./page.module.css";
+import { createReviewAvtion } from "@/actions/createReview.action";
 
 // generateStaticParams에서 return하는 값 외에는 전부 404로 보내기
 // export const dynamicParams = false;
@@ -42,31 +43,10 @@ async function BookDetail({ bookId }: { bookId: string }) {
 }
 
 const ReviewEditor = ({ bookId }: { bookId: string }) => {
-  async function createReviewAvtion(formData: FormData) {
-    "use server";
-    const content = formData.get("content")?.toString;
-    const author = formData.get("author")?.toString;
-
-    if (!content || !author) return;
-
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
-        {
-          method: "POST",
-          body: JSON.stringify({ bookId, content, author }),
-        }
-      );
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-      return;
-    }
-  }
-
   return (
     <section>
       <form action={createReviewAvtion}>
+        <input name="bookId" value={bookId} hidden />
         <input required type="text" name="content" placeholder="리뷰 내용" />
         <input required type="text" name="author" placeholder="작성자" />
         <button type="submit">작성하기</button>
