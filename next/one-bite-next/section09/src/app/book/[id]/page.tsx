d96e4a14133c21e834c37c2049810753898746d7
Minho Kang <rkdalsgh0106@naver.com>
2024-08-27 11:27:52 +0900

    Inmage 태그 이용해 이미지 최적화

--- next/one-bite-next/section09/src/app/book/[id]/page.tsx
+++ next/one-bite-next/section09/src/app/book/[id]/page.tsx
@@ -3,6 +3,7 @@ import style from "./page.module.css";
 import { ReviewData } from "@/types";
 import ReviewItem from "@/components/reviewItem";
 import { ReviewEditor } from "@/components/reviewEditor";
+import Image from "next/image";
 
 // generateStaticParams에서 return하는 값 외에는 전부 404로 보내기
 // export const dynamicParams = false;
@@ -11,7 +12,7 @@ export function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 
- async function BookDetail({ bookId }: { bookId: string }) {
+async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
     {
@@ -35,7 +36,12 @@ export function generateStaticParams() {
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
       >
-        <img src={coverImgUrl} />
+        <Image
+          src={coverImgUrl}
+          width={240}
+          height={300}
+          alt={`도서 ${title}의 커버 이미지`}
+        />
       </div>
       <div className={style.title}>{title}</div>
       <div className={style.subTitle}>{subTitle}</div>
