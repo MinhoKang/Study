7c0823daaf84ff0603547a150e992fdf7833a450
Minho Kang <rkdalsgh0106@naver.com>
2024-08-28 11:13:16 +0900

    todo-list 초기 설정

---
+++ next/todo-list/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import "./globals.css";
+import style from "./layout.module.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr">
+      <body>{children}</body>
+    </html>
+  );
+}
