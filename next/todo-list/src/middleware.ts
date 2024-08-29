721d9f85b2c263197d3e86b856c8c696d630e9aa
Minho Kang <rkdalsgh0106@naver.com>
2024-08-29 14:45:28 +0900

    middleware 설정

---
+++ next/todo-list/src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const middleware = (request: NextRequest) => {
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  if (!accessToken) return NextResponse.redirect(new URL("/", request.url));
+
+  if (accessToken && request.nextUrl.pathname === "/")
+    return NextResponse.redirect(new URL("/todo", request.url));
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ["/", "/todo/:path*"],
+};
