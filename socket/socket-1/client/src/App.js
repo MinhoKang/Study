ad2c2e4cd1c996542c9f48fc59208904f473aca5
Minho <rkdalsgh0106@naver.com>
2024-09-30 20:15:33 +0900

    방입장 기능

--- socket/socket-1/client/src/App.js
+++ socket/socket-1/client/src/App.js
@@ -5,11 +5,18 @@ import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
 
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
   const sendMessage = () => {
-    socket.emit("send_message", { message: message });
+    socket.emit("send_message", { message, room });
   };
 
   useEffect(() => {
@@ -20,6 +27,11 @@ function App() {
 
   return (
     <div className="App">
+      <input
+        placeholder="Room number"
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join Room</button>
       <input
         placeholder="Message..."
         onChange={(e) => setMessage(e.target.value)}
