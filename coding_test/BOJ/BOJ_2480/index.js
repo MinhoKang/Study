7188e8e4aad50d6520319ed721b94b25a85492f2
Minho <rkdalsgh0106@naver.com>
2024-10-26 16:20:38 +0900

    백준 2480

--- coding_test/BOJ/BOJ_2480/index.js
+++ coding_test/BOJ/BOJ_2480/index.js
@@ -18,3 +18,22 @@ if (first === second && first === third) {
 
   console.log(1000 + sameNum * 100);
 }
+
+// const fs = require("fs");
+// const input = fs.readFileSync("./input.txt").toString().split(" ").map(Number);
+
+// const [first, second, third] = input;
+
+// // 같은 숫자가 세 개일 경우
+// if (first === second && second === third) {
+//   console.log(10000 + first * 1000);
+// }
+// // 모두 다른 숫자일 경우
+// else if (first !== second && first !== third && second !== third) {
+//   console.log(Math.max(...input) * 100);
+// }
+// // 두 개의 숫자가 같은 경우
+// else {
+//   const sameNum = first === second || first === third ? first : second;
+//   console.log(1000 + sameNum * 100);
+// }
