c63cdc82581a5be6a5ab2bf30cd09a362f47221d
Minho <rkdalsgh0106@naver.com>
2024-10-26 16:16:42 +0900

    백준 2480

--- coding_test/BOJ/BOJ_2480/index.js
+++ coding_test/BOJ/BOJ_2480/index.js
@@ -1,13 +1,20 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().split(" ").map(Number);
 
-const A = input[0];
-const B = input[1];
-const C = input[2];
+const [first, second, third] = input;
 
-if (A === B && A === C && C === B) {
-  console.log(10000 + A * 1000);
-} else if (A !== B && A !== C && C !== B) {
-  
-  console.log(1000);
+if (first === second && first === third) {
+  console.log(10000 + first * 1000);
+} else if (first !== second && first !== third && second !== third) {
+  const large = Math.max(...input);
+  console.log(large * 100);
+} else {
+  const sameNum =
+    first === second
+      ? first
+      : second === third
+      ? second
+      : first === third && third;
+
+  console.log(1000 + sameNum * 100);
 }
