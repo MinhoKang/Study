2e9e3f42fb7a8f1132da1423a552042e1f907eb4
Minho <rkdalsgh0106@naver.com>
2024-10-29 19:16:31 +0900

    백준 2460번

---
+++ coding_test/BOJ/BOJ_2460/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((i) => i.split(" ").map(Number));
+
+const obj = input.reduce(
+  (a, c) => {
+    a.now = (a.now || 0) - c[0] + c[1];
+    if (a.max < a.now) {
+      a.max = a.now;
+    }
+
+    return a;
+  },
+  { now: 0, max: 0 }
+);
+console.log(obj.max.toString());
