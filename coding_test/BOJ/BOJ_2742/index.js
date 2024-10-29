38ab43245e494349eec7b1245014c127e0cdb55f
Minho <rkdalsgh0106@naver.com>
2024-10-29 21:49:43 +0900

    백준 2742

---
+++ coding_test/BOJ/BOJ_2742/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = +fs.readFileSync("./input.txt").toString().trim();
+
+const arr = Array.from({ length: input }).map((_, i) => i + 1);
+
+console.log(arr.reverse().join("\n"));
