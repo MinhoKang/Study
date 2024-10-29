efb3d28b1b6872afa3244ea4fb5bf1e28ee105a5
Minho <rkdalsgh0106@naver.com>
2024-10-29 20:24:57 +0900

    백준 2558번

---
+++ coding_test/BOJ/BOJ_2558/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const [A, B] = input;
+
+console.log(A + B);
