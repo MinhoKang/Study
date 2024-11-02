25b72bfaffb9ab5c7bffc253766fe4da3e04366c
Minho <rkdalsgh0106@naver.com>
2024-11-02 22:38:55 +0900

    백준 15740

---
+++ coding_test/BOJ/BOJ_15740/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(BigInt);
+
+const [A, B] = input;
+
+console.log((A + B).toString());
