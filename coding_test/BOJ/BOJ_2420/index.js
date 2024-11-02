137179aa620d9ed287a3cf639fecaaa3f38e89a1
Minho <rkdalsgh0106@naver.com>
2024-11-02 22:14:03 +0900

    백준 2420

---
+++ coding_test/BOJ/BOJ_2420/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const [N, M] = input;
+
+console.log(Math.abs(N - M));
