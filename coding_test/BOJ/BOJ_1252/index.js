850ef21b2ef60cd50be747e214a74e2a2346c42d
Minho <rkdalsgh0106@naver.com>
2024-11-03 14:10:04 +0900

    백준 1252

--- coding_test/BOJ/BOJ_1252/index.js
+++ coding_test/BOJ/BOJ_1252/index.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync("./input.txt")
-  .toString()
-  .trim()
-  .split(" ")
+const input = fs.readFileSync("./input.txt").toString().trim().split(" ");
 
 const [A, B] = input;
 
-const decimalA = parseInt(A, 2);
-const decimalB = parseInt(B, 2);
+const toBinary = (n) => {
+  if (n === 0) return "0";
+  if (n === 1) return "1";
+
+  return toBinary(Math.floor(n / 2)) + (n % 2);
+};
+
+const decimalA = BigInt("0b" + A, 2);
+const decimalB = BigInt("0b" + B, 2);
 
 const sum = decimalA + decimalB;
 
